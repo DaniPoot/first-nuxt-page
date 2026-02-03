e136e6fbe48413fe14d00fa706078863a5e17774
DaniPoot <danielpoot.uc2@gmail.com>
2026-02-02 19:18:14 -0600

    🔍️ Add seo tags on product page

--- app/pages/(public)/product/[slug].vue
+++ app/pages/(public)/product/[slug].vue
@@ -4,6 +4,15 @@ const slug = route.params.slug as string;
 
 const { product } = await useProduct(slug)
 
+useSeoMeta({
+  title: () => product.value?.name || 'Producto',
+  description: () => product.value?.description || '',
+  ogTitle: () => product.value?.name || 'Producto',
+  ogDescription: () => product.value?.description || '',
+  ogImage: () => product.value?.images?.[0] || '',
+  twitterTitle: () => product.value?.name || 'Producto'
+})
+
 // Si no se encuentra el producto, mostrar error 404
 if (!product.value) {
   navigateTo('/404')
